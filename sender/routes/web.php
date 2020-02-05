9f74df1e3fcee80b4b74426f31563d96e78d4dcc
Cristian <kryzale@live.com>
2020-02-05 09:46:17 -0500

    Clases de autenticacion agregadas

--- sender/routes/web.php
+++ sender/routes/web.php
@@ -5,3 +5,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
