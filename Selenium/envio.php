dd46cc0eca8d099cad14fb35318cdb0609333482
Cristian <kryzale@live.com>
2020-02-13 14:25:18 -0500

    Low Fixes

--- Selenium/envio.php
+++ Selenium/envio.php
@@ -18,7 +18,7 @@ $mensajesenviados=0;
 $mensajesnoenviados=0;
     
     $numeroTelefonico = '0979358929';
-    $mensaje ='esta es una prueba';
+    $mensaje ='Hola que mas amigo';
     $tiempoespera = 10;
     $intervalo = 10;
     
@@ -40,13 +40,17 @@ $mensajesnoenviados=0;
         sleep($tiempoespera);
         $botonAdjunto = $driver->findElement(WebDriverBy::cssSelector('#main > header > div._2kYeZ > div > div:nth-child(2) > div'));
         $botonAdjunto->click();
+        sleep(5);
         $fileInput = $driver->findElement(WebDriverBy::cssSelector('#main > header > div._2kYeZ > div > div._3j8Pd.GPmgf > span > div > div > ul > li:nth-child(1) > button > input[type=file]'));
         //$fileInput = $driver->findElement(WebDriverBy::cssSelector('#main > header > div._2kYeZ > div > div._3j8Pd.GPmgf > span > div > div > ul > li:nth-child(1) > button'));
         //$fileInput->click();
         $fileInput->setFileDetector(new LocalFileDetector());
         $filePath='C:\Users\KryzAle\Pictures\e3.jpg';
         $fileInput->sendKeys($filePath);
-        sleep($tiempoespera);
+        //sleep($tiempoespera);
+        $botonEnvioIMG = $driver->wait()->until(
+          WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('#app > div > div > div._2aMzp > div._10V4p._1jxtm > span > div > span > div > div > div.rK2ei.USE1O > span > div > div'))
+        );
         $botonEnvioIMG = $driver->findElement(WebDriverBy::cssSelector('#app > div > div > div._2aMzp > div._10V4p._1jxtm > span > div > span > div > div > div.rK2ei.USE1O > span > div > div'));
         $botonEnvioIMG->click();
         /*$elements = $driver->findElements(WebDriverBy::cssSelector('#main > footer > div._2i7Ej._14Mgc.copyable-area > div:nth-child(3) > button'));
