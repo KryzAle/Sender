48fa194300b9151b43c35255bf91461bb1aad085
KryzAle <kryzale@gmail.com>
2020-02-19 14:03:16 -0500

    Agregados archivos para el deploy

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+start chrome.exe http://thesender.test/home
+start chrome.exe https://web.whatsapp.com/ -remote-debugging-port=9014 --user-data-dir="c:\sesionChrome"
+start chromedriver.exe --port=4444
